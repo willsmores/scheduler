b24e681dfeeb11b99716b7bf9c83a161fb63f80a
Steve Willsmore <scwillsmore@gmail.com>
2023-01-11 17:38:56 -0500

    Add InterviewList components.

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import InterviewerListItem from './InterviewerListItem';
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+
+  const [interviewer, setInterviewer] = useState();
+
+  const interviewersParsed = props.interviewers.map(item =>
+    <InterviewerListItem
+      key={item.id}
+      name={item.name}
+      avatar={item.avatar}
+      selected={item.id === props.value}
+      setInterviewer={() => props.onChange(item.id)}
+    />
+  )
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewersParsed}
+      </ul>
+    </section>
+  );
+}
